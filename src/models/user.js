2d721a87d8e705747db19623ef4e26a529420827
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-10-15 08:28:28 +0530

    schema types and validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,46 @@
 const mongoose = require('mongoose') ;
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    emailId: String,
-    password: String,
-    age: Number,
-    gender: String
-});
+        firstName: {
+            type: String,
+            required: true,
+            maxLength: 20,
+        },
+        lastName: {
+            type: String,
+            maxLength: 20,
+        },
+        emailId: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 6
+        },
+        age: {
+            type: Number,
+            min: 18 
+        },
+        gender: {
+            type: String,
+            validate(value) {
+                if(!["male","female","others"].includes(value)) {
+                    throw new Error("Invalid Gender") ;
+                }
+            }
+        },
+    },
+    {
+        timestamps: true
+    },
+);
+
+
+
 //mongoose.model(modelName, schema)
 const UserModel = mongoose.model("User",userSchema) ;
 module.exports = UserModel ;
