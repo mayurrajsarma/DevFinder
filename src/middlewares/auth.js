01af72bc5df9fd6132944fd7788709f010ba619d
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-10-20 08:52:01 +0530

    auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,33 @@
+const jwt = require("jsonwebtoken") ;
+const User = require("../models/user") ;
+
 const userAuth = async (req,res,next)=> {
-    //Read the token from the req
-    const cookie = req.cookies ;
-    const {token} = cookie ;
+    try {
+        //Read the token from the req
+        const cookie = req.cookies ;
+        const {token} = cookie ;
+        if(!token) {
+            throw new Error("Invalid Token") ;   
+        }
+
+        //validate the token
+        const decodedObj = await jwt.verify(token,process.env.SECRET_KEY) ;
+        const {_id} = decodedObj ;
+        
+        //Find the user
+        const user = await User.findById({_id:_id}) ;
+        //token is valid but user doesnt exist in our db
+        if(!user) {
+            throw new Error("User does not exist") 
+        }
+        req.user = user ;
+        next() ;
+
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message) ;
+    }
 }
+
+module.exports = {
+    userAuth,
+};
