b1d172a02075f0cbed1fff9c7cf3bb4a4acb8f46
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-11-13 00:24:29 +0530

    profile api

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,16 +1,35 @@
 const express = require("express") ;
 const {userAuth} = require("../middlewares/auth") ;
+const {validateEditProfileData} = require("../utils/validation") ;
 
 const profileRouter = express.Router() ;
 
 //profile
-profileRouter.get("/profile",userAuth,async (req,res)=> {
-    try {
-       const user = req.user ;// from userAuth
-       res.send(user) ;
-    } catch (err) {
-       res.status(400).send("ERROR: " + err.message) ;
-    }
+profileRouter.get("/profile/view",userAuth,async (req,res)=> {
+   try {
+      const user = req.user ;// from userAuth
+      res.send(user) ;
+   }catch (err) {
+      res.status(400).send("ERROR: " + err.message) ;
+   }
  });
+
+profileRouter.patch("/profile/edit",userAuth,async(req,res)=> {
+   try {
+      if(!validateEditProfileData(req)) {
+         throw new Error("Invalid Edit Request") ;
+      }
+      const userToEdit = req.user ;
+      Object.keys(req.body).forEach(key => userToEdit[key] = req.body[key]) ; 
+      await userToEdit.save() ;
+      res.json({
+         message: `${userToEdit.firstName} your profile updated successfully`,
+         data: userToEdit,
+      }) ;
+
+   } catch (err) {
+      res.status(400).send("ERROR: " + err.message) ;   
+   }
+})
 
 module.exports = profileRouter ;
