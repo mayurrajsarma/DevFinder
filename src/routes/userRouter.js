099c70c17cc8a6c1340c1bbb5302209425079ac8
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-11-21 21:03:59 +0530

    user connections api

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -21,4 +21,33 @@ userRouter.get("/user/request/received",userAuth,async (req,res)=> {
     }
 })
 
+userRouter.get("/user/connection",userAuth,async (req,res)=> {
+    try {
+        const loginUser = req.user ;
+        const connections = await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loginUser._id,status:"accept"},
+                {toUserId:loginUser._id,status:"accept"},
+            ]
+        })
+        .populate("fromUserId", "firstName lastName")
+        .populate("toUserId", "firstName lastName")
+
+        const data = connections.map(rows=> {
+            //or we can use: rows.fromUserId._id.toString()===loginUser._id.toString()
+            if(rows.fromUserId._id.equals(loginUser._id)){
+                return rows.toUserId;
+            }
+            return rows.fromUserId ;
+        })
+
+        res.json({
+            message: "Data fetched successfully",
+            data: data,
+        });
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message) ;
+    }
+})
+
 module.exports = userRouter ;
